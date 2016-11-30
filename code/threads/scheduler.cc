c28d12d9589cc66b873b83112d0d70003c2cd999
PkuDavidGuan <1400012923@pku.edu.cn>
2016-11-30 08:55:51 +0000

    fix a bug. Clear the tlb before the switch.

--- code/threads/scheduler.cc
+++ code/threads/scheduler.cc
@@ -113,6 +113,10 @@ Scheduler::Run (Thread *nextThread)
     // a bit to figure out what happens after this, both from the point
     // of view of the thread and from the perspective of the "outside world".
 
+    #ifdef  USER_PROGRAM
+    for(int i = 0; i < TLBSize; ++i)
+        machine->tlb[i].valid = false;
+    #endif
     SWITCH(oldThread, nextThread);
     
     DEBUG('t', "Now in thread \"%s\"\n", currentThread->getName());
